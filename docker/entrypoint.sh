dbf2421ed199982fa30269f43766c2cf3ab03052
Jyrko <uriydemoshenko@gmail.com>
2025-04-09 21:08:57 +0200

    cleanup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Wait for postgres
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
@@ -8,10 +7,8 @@ done
 
 >&2 echo "Postgres is up - executing command"
 
-# Apply database migrations
 python manage.py migrate
 
-# Create superuser if needed
 python manage.py shell -c "
 from django.contrib.auth import get_user_model;
 User = get_user_model();
@@ -19,5 +16,4 @@ if not User.objects.filter(email='$DJANGO_SUPERUSER_EMAIL').exists():
     User.objects.create_superuser('$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
 "
 
-# Start Daphne
 exec daphne -b 0.0.0.0 -p 8000 TakeCare.asgi:application
