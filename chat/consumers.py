c09d7f152d55256b8bcfa2b77849114a28d16844
Jyrko <uriydemoshenko@gmail.com>
2025-04-09 20:58:13 +0200

    add websocket support. Add the support for asgi server

---
+++ chat/consumers.py
@@ -0,0 +1,63 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import sync_to_async
+from .models import ChatRoom, Message
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_id = self.scope['url_route']['kwargs']['room_id']
+        self.room_group_name = f'chat_{self.room_id}'
+        self.user = self.scope['user']
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Save message to database
+        await self.save_message(message)
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'sender_id': str(self.user.id),
+                'sender_name': self.user.name or f"User {self.user.id}"
+            }
+        )
+
+    async def chat_message(self, event):
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': event['message'],
+            'sender_id': event['sender_id'],
+            'sender_name': event['sender_name']
+        }))
+
+    @sync_to_async
+    def save_message(self, message):
+        chatroom = ChatRoom.objects.get(id=self.room_id)
+        Message.objects.create(
+            chatroom=chatroom,
+            sender=self.user,
+            content=message
+        )
